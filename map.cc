0a809dac16f75bbd06d7005024bebd8f164ea468
gregory_0974008 <JoshG4569@proton.me>
2025-11-07 13:52:02 -0800

    make test map (person in box)

--- map.cc
+++ map.cc
@@ -1,3 +1,24 @@
 #include <vector>
-#include "public/colors.h"
+#include "/public/colors.h"
 
+using namespace std;
+
+vector<string> world_map_test = {
+	"********************",
+	"*     +-------+    *",
+	"*     |   *   |    *",
+	"*     |  /|\\  |    *",
+	"*     |  / \\  |    *",
+	"*     +-------+    *",
+	"********************"
+};
+
+
+
+int main() {
+	for (int i = 0; i < world_map_test.size(); i++) {
+		cout << world_map_test.at(i) << endl;
+	}
+
+	return 0;
+}
