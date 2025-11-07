e505bb5209ee9979692a19e044b2280f45704ece
Elduderino90 <mendozadaniel1210@gmail.com>
2025-11-06 18:41:39 -0800

    Colors

--- main.cc
+++ main.cc
@@ -4,3 +4,26 @@
 //Extra Credit: 
 //URL to cover art and music: 
 
+
+#include </public/colors.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string BLUE;
+    string RED;
+    string YELLOW;
+    string GREEN;
+    string PURPLE;
+    string CYAN;
+
+    cout << BLUE << "Blue" << RESET << endl;
+    cout << RED << "Red" << RESET << endl;
+    cout << YELLOW << "Yellow" << RESET << endl;
+    cout << GREEN << "Green" <<  RESET << endl;
+    cout << PURPLE << "Purple" << RESET << endl;
+    cout << CYAN << "Cyan" << RESET << endl;
+}
+
+
