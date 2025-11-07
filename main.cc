df0d06fd28eb88e38b482601fc395c491043c6e7
Elduderino90 <mendozadaniel1210@gmail.com>
2025-11-06 18:55:32 -0800

    Updated

--- main.cc
+++ main.cc
@@ -24,6 +24,34 @@ int main() {
     cout << GREEN << "Green" <<  RESET << endl;
     cout << PURPLE << "Purple" << RESET << endl;
     cout << CYAN << "Cyan" << RESET << endl;
-}
+
+ BigScreen();
+    int choice;
+    while (true) {
+        showMenu();
+        cin >> choice;
+
+        if (choice == 1) {
+            cout << GREEN << "Let's Do This!" << RESET << "\n\n";
+            break;
+        }
+
+        else if (choice == 2) {
+            showControls();
+        }
+
+    else if (choice == 3) {
+        cout << RED << "Exiting Program.\n" << RESET;
+        return 0;
+    }
+        else (
+                cout << RED << "Invalid Choice,m try agaian.\n" << RESET;
+                }
+            }
+            cout << CYAN << "Your doomed....\n" << RESET;
+            cout << GREEN << "Something Is watching you.\n" << RESET;
+            COUT << YELLOW << "Your journey begins here.\n" << RESET;
+            return 0;
+        }
 
 
