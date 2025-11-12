d1211cbdbf346e51eda29aac7374136312cf56d2
gregory_0974008 <JoshG4569@proton.me>
2025-11-11 20:29:23 -0800

    im having a day

---
+++ mapLogic.cc
@@ -0,0 +1,8 @@
+#include <vector>
+#include "/public/colors.h"
+#include "mapData.cc"
+using namespace std;
+
+int main() {
+    //TODO once im not having a literal breakdown at this second im writing this
+}
